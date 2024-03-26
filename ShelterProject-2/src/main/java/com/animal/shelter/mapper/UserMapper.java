5c83f223ecf57e040c7d433332c2a1f972d7919d
leejiyong <rosdlwl005@naver.com>
2024-03-26 16:52:47 +0900

    로그인

--- ShelterProject-2/src/main/java/com/animal/shelter/mapper/UserMapper.java
+++ ShelterProject-2/src/main/java/com/animal/shelter/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.animal.shelter.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -10,5 +11,10 @@ import com.animal.shelter.vo.User;
 @Mapper
 public interface UserMapper {
 	 @Select("SELECT * FROM users")
-	    List<User> findAll();
+	 List<User> findAll();
+	 
+	 @Insert("INSERT INTO Users(name, email, password, contact) VALUES(#{name}, #{email}, #{password}, #{contact})")
+	 int insertUser(User user);
+	 
+	 
 }
--- ShelterProject-2/src/test/java/com/animal/shelter/ShelterProject2ApplicationTests.java
+++ ShelterProject-2/src/test/java/com/animal/shelter/ShelterProject2ApplicationTests.java
@@ -1,13 +1,28 @@
 package com.animal.shelter;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.animal.shelter.mapper.UserMapper;
+import com.animal.shelter.vo.User;
 
 @SpringBootTest
 class ShelterProject2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private UserMapper userMapper;
 
+	@Test
+	public void findAllTest() {
+		 List<User> users = userMapper.findAll();
+		    assertThat(users).isNotNull();
+		    if (!users.isEmpty()) {
+		        assertThat(users.size()).isGreaterThan(0);
+		    }
+	}
 }

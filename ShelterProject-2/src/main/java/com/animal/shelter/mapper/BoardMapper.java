8a8b2b3923a3e2d3e2760d3f66da52752c574909
leejiyong <rosdlwl005@naver.com>
2024-03-30 21:40:36 +0900

    추가

---
+++ ShelterProject-2/src/main/java/com/animal/shelter/mapper/BoardMapper.java
@@ -0,0 +1,10 @@
+package com.animal.shelter.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BoardMapper {
+	
+	
+
+}
---
+++ ShelterProject-2/src/main/java/com/animal/shelter/mapper/CommentMapper.java
@@ -0,0 +1,8 @@
+package com.animal.shelter.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CommentMapper {
+
+}

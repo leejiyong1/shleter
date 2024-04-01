9b6855681be7f980e9407f1ffbce24f5e59bd635
leejiyong <rosdlwl005@naver.com>
2024-04-01 20:40:34 +0900

    컨트롤러

---
+++ ShelterProject-2/src/main/java/com/animal/shelter/controller/BoardController.java
@@ -0,0 +1,15 @@
+package com.animal.shelter.controller;
+
+import org.springframework.stereotype.Controller;
+
+import com.animal.shelter.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class BoardController {
+	
+	private final BoardService boardService;
+
+}
---
+++ ShelterProject-2/src/main/java/com/animal/shelter/controller/CommentController.java
@@ -0,0 +1,17 @@
+package com.animal.shelter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.animal.shelter.service.CommentService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/comment")
+@RequiredArgsConstructor
+public class CommentController {
+	
+	private final CommentService commentService;
+
+}
